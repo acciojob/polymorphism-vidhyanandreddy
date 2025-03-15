055a791eff802563385ec66108e212f204015ade
vidhyanandreddy <vidhyanand067@gmail.com>
2025-03-15 22:50:33 +0530

    Product Class added

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,19 +1,26 @@
 package com.driver;
 
 public class Main {
+    // Task 1: Create a class Product inside Main
     static class Product {
+        // Task 3: Create a method with two integer parameters
         public int product(int x, int y) {
             return x * y;
         }
+
+        // Task 4: Create an overloaded method with three integer parameters
         public int product(int x, int y, int z) {
             return x * y * z;
         }
+
+        // Task 5: Create an overloaded method with two double parameters
         public double product(double x, double y) {
             return x * y;
         }
     }
 
     public static void main(String[] args) {
+        // Task 2: Create an object of Product in Main function
         Product p = new Product();
 
         // Calling the methods and displaying results
