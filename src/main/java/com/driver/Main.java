416bbd093415480700603a0b71c58d04d671e1f4
vidhyanandreddy <vidhyanand067@gmail.com>
2025-03-15 22:46:47 +0530

    Product Class added

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,24 @@
 package com.driver;
 
 public class Main {
+    static class Product {
+        public int product(int x, int y) {
+            return x * y;
+        }
+        public int product(int x, int y, int z) {
+            return x * y * z;
+        }
+        public double product(double x, double y) {
+            return x * y;
+        }
+    }
 
+    public static void main(String[] args) {
+        Product p = new Product();
+
+        // Calling the methods and displaying results
+        System.out.println("Product of 2 and 3: " + p.product(2, 3));
+        System.out.println("Product of 2, 3, and 4: " + p.product(2, 3, 4));
+        System.out.println("Product of 2.5 and 3.5: " + p.product(2.5, 3.5));
+    }
 }
