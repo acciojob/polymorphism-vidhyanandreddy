3e47526061124a9774b340ed22cde14cfc674fb9
vidhyanandreddy <vidhyanand067@gmail.com>
2025-03-17 20:09:17 +0530

    Assignment completed

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -5,6 +5,7 @@ public class Main {
         public int product(int x, int y) {
             return x * y;
         }
+
         public int product(int x, int y, int z) {
             return x * y * z;
         }
